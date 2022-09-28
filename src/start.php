880b6a827b0de7d9bf879d6401d1f1ca3a920f15
Barney Laurance <barney@redmagic.org.uk>
2022-09-28 02:07:56 +0100

    Add argument handling

--- src/start.php
+++ src/start.php
@@ -4,16 +4,28 @@ namespace Bdsl\OnlyOne;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(name: 'start')]
 class Start extends Command
 {
+    protected function configure()
+    {
+        $this->setDefinition(new InputDefinition([
+            new InputArgument("resource", InputArgument::REQUIRED, "The name of the resource on which to acquire a lock"),
+            new InputArgument("repository", InputArgument::REQUIRED, "URI of the git repository to use as a distributed lock store"),
+        ]));
+    }
+
     public function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('Testing, testing');
-        $output->writeln("1, 2, 3");
+        $resourceName = $input->getArgument('resource');
+        $repositoryUrl = $input->getArgument("repository");
+
+        $output->writeln("So you want a lock for $resourceName at $repositoryUrl?");
 
         return 0;
     }
